1485681a42f11c82c3f3898427eaf5423b447c1d
Vizcarra Diego <310048109@cart.ads>
2018-04-06 15:27:11 -0700

    Getting ready for media queries

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-var len = document.forms[0].length - 1;
-var ele = document.forms[0].elements;
-
+var i = 0;
 function formValidate() {
+    len = document.forms[0].length - 1;
+    ele = document.forms[0].elements;
     console.log(ele);
     for (var i = 0; i < len; i++){
         if(ele[i].value == ""){
@@ -12,6 +12,7 @@ function formValidate() {
         }
     }
     checkPhone();
+    checkEmail();
 }
 function checkPhone(){
     if(!/\d[0-9]/.test(ele[1].value)){
@@ -20,6 +21,29 @@ function checkPhone(){
     }
     if (ele[1].value.length < 10 || ele[1].value.length > 10) {
         document.getElementById("paragraph").innerHTML = "Please use only 10 numbers within in the Phone area.";
+        return false;
+    }
+}
+function checkEmail(){
+    if(!/@/.test(ele[2].value)){
+        document.getElementById("paragraph").innerHTML = "Make sure to use an @ symbol in the email area.";
+        return false;
+    }
+    if(!/com/.test(ele[2].value)){
+        document.getElementById("paragraph").innerHTML = "Make sure to add a .com to the end of the address in the email area.";
+        return false;
+    }
+}
+function menuExpand(){
+    if((i % 2) == 0){
+       document.getElementById("me").getElementsByTagName("nav")[0].style.display = "contents";
+        document.getElementById("me").getElementsByTagName("button")[0].style.top = "5px";
+        i++;
+        return false;
+    } else{
+        document.getElementById("me").getElementsByTagName("nav")[0].style.display = "none";
+        document.getElementById("me").getElementsByTagName("button")[0].style.top = "60px";
+        i++;
         return false;
     }
 }
